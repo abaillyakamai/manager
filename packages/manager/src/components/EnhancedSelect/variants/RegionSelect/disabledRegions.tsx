496ce5ce2f95eb3195326a017122e00abbd23a24
Alban Bailly <abailly@akamai.com>
2023-10-17 10:00:28 -0400

    Gentle refactor/cleanup of disabled regions utils

--- packages/manager/src/components/EnhancedSelect/variants/RegionSelect/disabledRegions.tsx
+++ packages/manager/src/components/EnhancedSelect/variants/RegionSelect/disabledRegions.tsx
@@ -2,40 +2,32 @@ import * as React from 'react';
 
 import { Link } from 'src/components/Link';
 import { Typography } from 'src/components/Typography';
+import { regionFactory } from 'src/factories';
 
-import type { Capabilities, Region, RegionStatus } from '@linode/api-v4';
+import type { Region } from '@linode/api-v4';
 
 // DATA
-const tokyoDisabledMessage = (
-  <Typography>
-    Tokyo is sold out while we expand our capacity. We recommend deploying
-    workloads in Osaka.{` `}
-    <br />
-    <Link to="https://www.linode.com/blog/linode/tokyo-region-availability-update/">
-      Learn more
-    </Link>
-    .
-  </Typography>
-);
-
-const fakeTokyo: FakeRegion = {
-  capabilities: ['Linodes', 'NodeBalancers'] as Capabilities[],
-  country: 'jp',
-  disabled: true,
-  display: 'Tokyo, JP',
-  id: 'ap-northeast',
-  label: 'Tokyo, JP',
-  resolvers: {
-    ipv4:
-      '173.230.129.5,173.230.136.5,173.230.140.5,66.228.59.5,66.228.62.5,50.116.35.5,50.116.41.5,23.239.18.5',
-    ipv6: '',
-  },
-  status: 'ok' as RegionStatus,
-};
-
-type FakeRegion = Region & { disabled: boolean; display: string };
+const TOKYO_DISABLED_MESSAGE =
+  'Tokyo is sold out while we expand our capacity. We recommend deploying workloads in Osaka.';
+const TOKYO_DISABLED_MESSAGE_LINK =
+  'https://www.linode.com/blog/linode/tokyo-region-availability-update/';
 
 // UTILS
+interface DisabledMessage {
+  disabledLink: string;
+  disabledMessage: string;
+}
+
+const disabledMessage = ({
+  disabledLink,
+  disabledMessage,
+}: DisabledMessage): JSX.Element => (
+  <Typography>
+    {disabledMessage}
+    <br />
+    <Link to={disabledLink}>Learn more</Link>.
+  </Typography>
+);
 interface DisabledRegion {
   /**
    * The message to display when the region is disabled.
@@ -48,7 +40,7 @@ interface DisabledRegion {
   /**
    * The fake region to display.
    */
-  fakeRegion: FakeRegion;
+  fakeRegion: Region;
   /**
    * The feature flag that controls whether the fake region should be displayed.
    */
@@ -57,9 +49,15 @@ interface DisabledRegion {
 
 export const listOfDisabledRegions: DisabledRegion[] = [
   {
-    disabledMessage: tokyoDisabledMessage,
+    disabledMessage: disabledMessage({
+      disabledLink: TOKYO_DISABLED_MESSAGE_LINK,
+      disabledMessage: TOKYO_DISABLED_MESSAGE,
+    }),
     excludePaths: ['/object-storage/buckets/create'],
-    fakeRegion: fakeTokyo,
+    fakeRegion: regionFactory.build({
+      id: 'ap-northeast',
+      label: 'Tokyo, JP',
+    }),
     featureFlag: 'soldOutTokyo',
   },
 ];
